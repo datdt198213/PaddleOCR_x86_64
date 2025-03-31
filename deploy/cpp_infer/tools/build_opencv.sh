23a293b4513fbfa75ba14028959d28c529669b32
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2025-04-01 00:07:25 +0700

    Update build_opencv.sh on x86_64 machine

--- deploy/cpp_infer/tools/build_opencv.sh
+++ deploy/cpp_infer/tools/build_opencv.sh
@@ -26,3 +26,32 @@ cmake .. \
 
 make -j
 make install
+
+
+# cmake_minimum_required(VERSION 3.5)
+# set(CMAKE_SYSTEM_NAME Linux)
+# set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+# set(target_arch aarch64-linux-gnu)
+# set(CMAKE_LIBRARY_ARCHITECTURE ${target_arch} CACHE STRING "" FORCE)
+
+# # specify the toolchain programs
+# find_program(CMAKE_C_COMPILER ${target_arch}-gcc)
+# find_program(CMAKE_CXX_COMPILER ${target_arch}-g++ NO_CACHE )
+# if(NOT CMAKE_C_COMPILER OR NOT CMAKE_CXX_COMPILER)
+#     message(FATAL_ERROR "Can't find suitable C/C++ cross compiler for ${target_arch}")
+# endif()
+
+# set(CMAKE_AR ${target_arch}-ar CACHE FILEPATH "" FORCE)
+# set(CMAKE_RANLIB ${target_arch}-ranlib)
+# set(CMAKE_LINKER ${target_arch}-ld)
+
+# # Not all shared libraries dependencies are instaled in host machine.
+# # Make sure linker doesn't complain.
+# set(CMAKE_EXE_LINKER_FLAGS_INIT -Wl,--allow-shlib-undefined)
+
+# # instruct nvcc to use our cross-compiler
+# set(CMAKE_CUDA_FLAGS "-ccbin ${CMAKE_CXX_COMPILER} -Xcompiler -fPIC" CACHE STRING "" FORCE)
+
+# SET(TARGET_FS "/l4t/targetfs/")
+# SET(SMS "87")
