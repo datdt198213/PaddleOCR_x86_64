a4d9f8c54db858bd17239695401758549dcdd12f
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2025-03-31 15:33:37 +0700

    Update build.sh in x86_64 machine

--- deploy/cpp_infer/tools/build.sh
+++ deploy/cpp_infer/tools/build.sh
@@ -1,21 +1,24 @@
-OPENCV_DIR=your_opencv_dir
-LIB_DIR=your_paddle_inference_dir
-CUDA_LIB_DIR=your_cuda_lib_dir
-CUDNN_LIB_DIR=your_cudnn_lib_dir
+PADDLE_LIB=/home/anlab/work_dir/x86_64/PaddleOCR/deploy/cpp_infer/paddle_inference/paddle_inference_openblas
+OPENCV_DIR=/home/anlab/work_dir/x86_64/PaddleOCR/deploy/cpp_infer/opencv-3.4.7/opencv3
+CUDA_LIB_DIR=/usr/local/cuda/lib64
+CUDNN_LIB_DIR=/usr/lib/x86_64-linux-gnu
+TENSORRT_DIR=/usr/lib/x86_64-linux-gnu
 
 BUILD_DIR=build
 rm -rf ${BUILD_DIR}
 mkdir ${BUILD_DIR}
 cd ${BUILD_DIR}
 cmake .. \
-    -DPADDLE_LIB=${LIB_DIR} \
-    -DWITH_MKL=ON \
-    -DWITH_GPU=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPADDLE_LIB=${PADDLE_LIB} \
+    -DWITH_MKL=OFF \
     -DWITH_STATIC_LIB=OFF \
+    -DWITH_GPU=ON \
     -DWITH_TENSORRT=OFF \
     -DOPENCV_DIR=${OPENCV_DIR} \
     -DCUDNN_LIB=${CUDNN_LIB_DIR} \
     -DCUDA_LIB=${CUDA_LIB_DIR} \
-    -DTENSORRT_DIR=${TENSORRT_DIR} \
+    -DTENSORRT_DIR=${TENSORRT_DIR}
+    # -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_AARCH64_DIR}
 
 make -j
